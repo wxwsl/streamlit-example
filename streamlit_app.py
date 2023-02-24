b1eaa3e0ecada86500a4fb09eaae52b14e08ae9f
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 07:20:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,11 +5,11 @@ import streamlit as st
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred=os.environ.get("FIREBASE_KEY")
-
 
 from google.oauth2 import service_account
 
+
+# Get Firebase credentials from environment variable
 firebase_key_json = json.loads(os.environ.get("FIREBASE_KEY"))
-firebase_credentials = service_account.Credentials.from_service_account_info(firebase_key_json)
+firebase_credentials = credentials.Certificate(private_key)
 st.write(cred)
