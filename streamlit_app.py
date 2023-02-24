79c4ffe47ac35c83d82d64f9343a9a99f3cfc504
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-24 20:23:49 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
-import json
-import streamlit as st
+import firebase_admin
+from firebase_admin import credentials
 
-
-# 显示 JSON 数据
-st.write(" 显示 JSON 数据")
+# 初始化 Firebase 应用程序
+cred = credentials.Certificate("mydata-7c783-77425a396005.json")
+try:
+    firebase_admin.initialize_app(cred)
+except ValueError:
+    pass
