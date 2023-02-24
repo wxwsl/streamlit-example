aa67d9793aeb30601c48ab6197d0dab95027619c
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 06:53:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,32 +8,5 @@ from firebase_admin import db
 firebase_key_json = json.loads(os.environ.get("FIREBASE_KEY"))
 cred = service_account.Credentials.from_service_account_info(firebase_key_json)
 
-#firebase_key = os.environ.get("FIREBASE_KEY")
-#cred = credentials.Certificate(firebase_key)
-try:
-    firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://mydata-7c783-default-rtdb.firebaseio.com'
-})
-except ValueError:
-    pass
 
-# 写入数据
-ref = db.reference('/')
-ref.set({
-    'group1': {
-        'name': 'Group 1',
-        'description': 'This is group 1'
-    },
-    'group2': {
-        'name': 'Group 2',
-        'description': 'This is group 2'
-    }
-})
-
-import streamlit as st
-import pandas as pd
-
-
-data = ref.get()
-df = pd.DataFrame.from_dict(data, orient='index')
-st.write(df)
+st.write(cred)
