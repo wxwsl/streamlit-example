ee4e5947124043749b6ea99e358cdb1e9cd4e7b6
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-24 20:13:12 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,6 @@
 import json
 import streamlit as st
 
-# 加载 JSON 文件
-with open("mydata-7c783-77425a396005.json") as f:
-    data = json.load(f)
 
 # 显示 JSON 数据
-st.write(data)
+st.write(" 显示 JSON 数据")
