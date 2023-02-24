fc98f9b636975e2d99da661569b4d015dd04dc39
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-24 20:07:18 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,4 @@ from firebase_admin import db
 
 # 初始化 Firebase 服务帐户凭据
 cred = credentials.Certificate("mydata-7c783-77425a396005.json")
-default_app = firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://mydata-7c783.firebaseio.com'
-})
+default_app = firebase_admin.initialize_app(cred)
