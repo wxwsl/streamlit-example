db6b5f53c978bebb63558d3a12e6f4be40384105
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-24 21:02:34 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
+import os
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-# 初始化 Firebase 应用程序
-cred = credentials.Certificate("mydata-7c783-77425a396005.json")
+firebase_key = os.environ.get("FIREBASE_KEY")
+cred = credentials.Certificate(firebase_key)
 try:
     firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://mydata-7c783-default-rtdb.firebaseio.com'
