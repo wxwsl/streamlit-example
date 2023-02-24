c5985181ce3580f9982163a0a2135b72510f1e78
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-24 19:46:30 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,5 @@ from firebase_admin import db
 # 初始化 Firebase 服务帐户凭据
 cred = credentials.Certificate("mydata-7c783-77425a396005.json")
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://mydata-7c783-default-rtdb.firebaseio.com'
+    'databaseURL': 'https://mydata-7c783.firebaseio.com'
 })
-# 连接到 Realtime Database
-ref = db.reference('/')
-
-# 读取数据
-data = ref.get()
