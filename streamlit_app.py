f79eda8b2c5e67125ff01e3e5e9b568e2e34bbed
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 09:47:46 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from streamlit.server.server import Server
-
+from streamlit.components.v1 import html
+import json
 
 # Define JavaScript code to get user location
 jscode = """
@@ -11,22 +11,37 @@ navigator.geolocation.getCurrentPosition(
         const accuracy  = position.coords.accuracy;
         const location  = {'latitude': latitude, 'longitude': longitude, 'accuracy': accuracy};
         const locationJson = JSON.stringify(location);
-        window.parent.postMessage({ location: locationJson }, '*');
+        const event = new CustomEvent("location", {detail: locationJson});
+        document.dispatchEvent(event);
     }
 );
 """
 
 # Display the JavaScript code using the html component
-st.components.v1.html('<script>{}</script>'.format(jscode))
+html_code = html('<script>{}</script>'.format(jscode))
 
-# Wait for the user to share their location
-location_json = Server.get_current()._location_request.future.result()
+# Define a function to handle custom events sent from JavaScript
+@st.cache(allow_output_mutation=True)
+def handle_custom_event():
+    location_json = st.session_state.location
+    if location_json:
+        location = json.loads(location_json)
+        st.write('Latitude:', location['latitude'])
+        st.write('Longitude:', location['longitude'])
+        st.write('Accuracy:', location['accuracy'])
+    else:
+        st.write('No location data received.')
 
-# Parse the JSON data and display it in Streamlit
-if location_json:
-    location = json.loads(location_json['args']['location'])
-    st.write('Latitude:', location['latitude'])
-    st.write('Longitude:', location['longitude'])
-    st.write('Accuracy:', location['accuracy'])
-else:
-    st.write('No location data received.')
+# Add a report context to store session state
+add_report_ctx = st.report_thread.add_report_ctx
+with add_report_ctx('session state'):
+    if 'location' not in st.session_state:
+        st.session_state.location = None
+
+# Wait for the custom event to be triggered
+document_event = st.bokeh_events().event('location')
+if document_event:
+    st.session_state.location = document_event['location']
+
+# Handle the custom event
+handle_custom_event()
