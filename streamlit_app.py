babce181a5c82027deea73ed65fc9b75de17964f
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 11:17:00 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,28 +1,11 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <meta charset="UTF-8">
-  <title>获取地理位置</title>
-</head>
-<body>
-  <p>点击下面的按钮获取您的位置：</p>
-  <button onclick="getLocation()">获取位置</button>
-  <p id="location"></p>
-  
-  <script>
-    function getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-      } else {
-        document.getElementById("location").innerHTML = "您的浏览器不支持获取位置信息。";
-      }
-    }
+import ipinfo
 
-    function showPosition(position) {
-      var latitude = position.coords.latitude;
-      var longitude = position.coords.longitude;
-      document.getElementById("location").innerHTML = "您的位置是：" + latitude + ", " + longitude;
-    }
-  </script>
-</body>
-</html>
+access_token = "fd7290568d6d4f"  # 替换成你自己的 access token
+handler = ipinfo.getHandler(access_token)
+
+details = handler.getDetails()
+latitude = details.latitude
+longitude = details.longitude
+
+print("Latitude:", latitude)
+print("Longitude:", longitude)
