8d70d3f9b446d08c5c6d437f08c481450abe3419
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 08:43:33 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -68,7 +68,7 @@ if st.button('Add'):
 import pandas as pd
 
 
-data = ref.get()
-df = pd.DataFrame.from_dict(data, orient='index')
-st.write(df)
+#data = ref.get()
+#df = pd.DataFrame.from_dict(data, orient='index')
+#st.write(df)
 
