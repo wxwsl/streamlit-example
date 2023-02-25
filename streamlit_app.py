e2496d35d85231516b88a23e4fb960aa57628aeb
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 09:45:32 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from streamlit.components.v1 import html
-import json
+from streamlit.server.server import Server
+
 
 # Define JavaScript code to get user location
 jscode = """
@@ -11,35 +11,22 @@ navigator.geolocation.getCurrentPosition(
         const accuracy  = position.coords.accuracy;
         const location  = {'latitude': latitude, 'longitude': longitude, 'accuracy': accuracy};
         const locationJson = JSON.stringify(location);
-        window.location.href = "streamlit://location=" + encodeURIComponent(locationJson);
+        window.parent.postMessage({ location: locationJson }, '*');
     }
 );
 """
 
 # Display the JavaScript code using the html component
-html_code = html('<script>{}</script>'.format(jscode))
+st.components.v1.html('<script>{}</script>'.format(jscode))
 
-# Define a callback function to handle the custom message
-@st.cache(allow_output_mutation=True)
-def get_location():
-    return {}
+# Wait for the user to share their location
+location_json = Server.get_current()._location_request.future.result()
 
-def handle_message(msg):
-    if 'location' in msg:
-        location = json.loads(msg['location'])
-        get_location()['location'] = location
-
-# Register the callback function with Streamlit
-st._legacy_chrome.message_loop.register_callback("location", handle_message)
-
-# Display the JavaScript code and wait for the user to share their location
-st.components.v1.html(html_code)
-
-# Wait for the location data to be available
-location_data = get_location()['location']
-if location_data:
-    st.write('Latitude:', location_data['latitude'])
-    st.write('Longitude:', location_data['longitude'])
-    st.write('Accuracy:', location_data['accuracy'])
+# Parse the JSON data and display it in Streamlit
+if location_json:
+    location = json.loads(location_json['args']['location'])
+    st.write('Latitude:', location['latitude'])
+    st.write('Longitude:', location['longitude'])
+    st.write('Accuracy:', location['accuracy'])
 else:
     st.write('No location data received.')
