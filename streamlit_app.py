dc1e9cfe5334b7a7229c0c2230d8bdd3adcb3c64
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 11:43:20 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import requests
-
+import streamlit as st
 ip_request = requests.get('https://ipapi.co/ip/')
 client_ip = ip_request.text
 print("Client IP Address:", client_ip)
+
+st.write(client_ip)
