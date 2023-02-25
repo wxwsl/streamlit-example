009bcc4205abba9a5d9d01be1edc5a7fc761c907
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 11:32:35 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
-from flask import request
+import requests
 
-client_ip = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
+ip_request = requests.get('https://ipapi.co/ip/')
+client_ip = ip_request.text
 print("Client IP Address:", client_ip)
