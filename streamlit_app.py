2b314affd7481cb6afde964cb762a4949c614859
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 09:33:52 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,87 +1,31 @@
-import os
-import firebase_admin
-import json
 import streamlit as st
-from firebase_admin import credentials
-from firebase_admin import db
+from streamlit.components.v1 import html
 
-
-from google.oauth2 import service_account
-
-
-# Get Firebase credentials from environment variable
-
-firebase_key = os.environ.get("FIREBASE_KEY")
-cert_dict = json.loads(firebase_key)
-cred = credentials.Certificate(cert_dict)
-
-
-try:
-    firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://mydata-7c783-default-rtdb.firebaseio.com'
-})
-except ValueError:
-    pass
-
-# 写入数据
-ref = db.reference('/')
-###ref.set({
-#    'group1': {
- #######       'name': 'Group 1',
-#        'description': 'This is group 1'
- #####   },
- ######   'group2': {
-  ####      'name': 'Group 2',
-  ###      'description': 'This is group 2'
-   ## }
-#})
-import datetime
-
-# 获取当前时间
-now = datetime.datetime.now()
-
-# 将时间转换为字符串
-time_str = now.strftime("%Y-%m-%d %H:%M:%S")
-
-
-# 创建输入文本框
-st.header('Add new data')
-name = st.text_input('Name')
-age = st.number_input('Age', min_value=0, max_value=120)
-email = st.text_input('Email')
-
-# Define behavior when user clicks the "Add" button
-if st.button('Add'):
-    # Define new data to be added
-    new_data = {
-        'name': name,
-        'age': age,
-        'email': email
+# Define JavaScript code to get user location
+jscode = """
+navigator.geolocation.getCurrentPosition(
+    function(position) {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const accuracy  = position.coords.accuracy;
+        const location  = {'latitude': latitude, 'longitude': longitude, 'accuracy': accuracy};
+        const locationJson = JSON.stringify(location);
+        window.location.href = "streamlit://location=" + encodeURIComponent(locationJson);
     }
+);
+"""
 
-    # Push the new data to the database with a new unique key
-    new_ref = ref.push(new_data)
-    st.success('New data added with key: {}'.format(new_ref.key))
+# Display the JavaScript code using the html component
+html_code = html('<script>{}</script>'.format(jscode))
 
+# Wait for the user to share their location
+location_json = st.wait_for_custom_request('location')
 
-
-
-
-
-import pandas as pd
-data = ref.get()
-
-if isinstance(data, dict):
-    data_dict = json.loads(json.dumps(data))
-    if data_dict:
-        for key, value in data_dict.items():
-            st.write('Key:', key)
-            st.write('Name:', value['name'])
-            st.write('Age:', value['age'])
-            st.write('Email:', value['email'])
-            st.write('---')
-    else:
-        st.write('No data found')
+# Parse the JSON data and display it in Streamlit
+if location_json:
+    location = json.loads(location_json['args']['location'])
+    st.write('Latitude:', location['latitude'])
+    st.write('Longitude:', location['longitude'])
+    st.write('Accuracy:', location['accuracy'])
 else:
-    st.write('No data found')
-
+    st.write('No location data received.')
