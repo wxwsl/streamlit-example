05d6a2d80c15e754e8161dd3ed781fa1a207e0db
wxwsl <71358906+wxwsl@users.noreply.github.com>
2023-02-25 11:25:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,4 @@
-import ipinfo
+from flask import request
 
-import requests
-
-def get_client_ip():
-    headers = requests.utils.default_headers()
-    headers.update({'User-Agent': 'Mozilla/5.0'})
-    ip_request = requests.get('https://api.ipify.org', headers=headers)
-    return ip_request.text
-
-client_ip = get_client_ip()
+client_ip = request.environ.get('HTTP_X_FORWARDED_FOR') or request.environ.get('REMOTE_ADDR')
 print("Client IP Address:", client_ip)
-
-
-
